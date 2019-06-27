8b4f7e6e03a6e9c21dcc05052eaf51ce5f600a4c
Santosh Kumar <sntshkmr60@gmail.com>
2019-06-27 15:12:55 +0530

    Spawn new terminal with tmux

--- common.sh
+++ common.sh
@@ -19,5 +19,12 @@ export PATH=$PATH:$HOME/.go/bin
 
 export LD_PRELOAD=/usr/lib64/libstdc++.so.6:/lib64/libgcc_s.so.1
 
+# Launch tmux as soon as shell starts
+[[ $TERM != "screen"  ]] && exec tmux
+
+# As we are inside tmux, let terminal support 256 colors,
+# so vim can show colorschemes properly inside tmux
+if [[ $TERM == screen  ]]; then TERM=screen-256color; fi
+
 # You may also want to source the completion script (for bash):
 # source /home/sntshk/rez-packages/completion/complete.sh
