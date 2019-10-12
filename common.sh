88cb930ca1ae9f3dd757b0d18b27423732da5a08
Santosh Kumar <sntshkmr60@gmail.com>
2019-10-12 06:24:35 +0530

    Adds snap bin dir to path

--- common.sh
+++ common.sh
@@ -16,6 +16,7 @@ export PATH="$HOME/anaconda3/bin:$PATH"
 
 export GOPATH=$HOME/.go
 export PATH=$PATH:$HOME/.go/bin
+export PATH=$PATH:/var/lib/snapd/snap/bin
 
 export LD_PRELOAD=/usr/lib64/libstdc++.so.6:/lib64/libgcc_s.so.1
 
