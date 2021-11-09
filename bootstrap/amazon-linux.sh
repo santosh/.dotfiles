f9193995d504a8370c726f6227715fb7e673ca78
Santosh Kumar <sntshkmr60@gmail.com>
2021-11-09 15:28:58 +0530

    Installation for Docker, Bat and Go is now in procs

--- bootstrap/amazon-linux.sh
+++ bootstrap/amazon-linux.sh
@@ -3,6 +3,56 @@
 # Note: This script is intended to run in an interactive shell. 
 # Use this script to create fresh bootstrapped image.
 
+# Headings
+# 1. Procedures
+# 2. Installation
+
+# Install latest golang
+function installLatestGo() {
+   curl -LO https://golang.org/dl/go1.17.3.linux-amd64.tar.gz
+   sudo tar -C /usr/local/ -xzf go1.17.3.linux-amd64.tar.gz
+   rm go1.17.3.linux-amd64.tar.gz
+}
+
+# Install and setup docker
+function installDocker() {
+    # Mounting the efs is the first thing you should do before starting the daemon in production environment.
+    sudo yum install docker -y -q
+    sudo echo '{ "data-root": "/efs/system/docker/data-root"  }' > /etc/docker/daemon.json
+
+    if [ $(getent group docker) ]; then
+        echo "docker group already exists; skipping."
+    else
+        sudo groupadd docker
+    fi
+
+    sudo usermod -aG docker ec2-user
+    sudo newgrp docker
+
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+    echo Done installing docker
+
+    sudo systemctl start docker
+    sudo systemctl enable docker
+}
+
+# Install latest bat
+function installBat() {
+    # Install bat
+    cd /tmp
+    LATEST_BAT_64_BIT=$(curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | \
+        grep browser_download_url | grep 'x86_64' | grep musl | \
+        cut -d '"' -f 4)
+    curl -sLO $LATEST_BAT_64_BIT
+    tar -xvf bat* && cd bat*
+    mkdir -p ~/bin && mv bat ~/bin
+    cd ~ && rm -r /tmp/bat*
+    echo Done install bat.
+}
+
 { set +x; } 2>/dev/null
 
 sudo yum update -y -q
@@ -11,30 +61,13 @@ sudo yum upgrade -y -q
 # Install initial tools
 sudo yum group install 'Development Tools' -y -q
 sudo amazon-linux-extras install epel -y -q
-sudo yum install vim-X11 golang docker tmux tree python3 git-lfs htop -y -q
+sudo yum install vim-X11 tmux tree python3 git-lfs htop -y -q
 echo Done installing packages.
 
-# Configure initial tools
-sudo echo '{ "data-root": "/efs/system/docker/data-root"  }' > /etc/docker/daemon.json
-# Mounting the efs is the first thing you should do before starting the daemon in production environment.
-
-sudo systemctl start docker
-sudo systemctl enable docker
-
-if [ $(getent group docker) ]; then
-    echo "docker group already exists; skipping."
-else
-    sudo groupadd docker
-fi
-
-sudo usermod -aG docker ec2-user
-sudo newgrp docker
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-echo Done installing docker
+# invoke function (don't need paren)
+installLatestGo
+installDocker
+installBat
 
 # Set upper limit to journalctl
 journalctl --vacuum-time=180d
@@ -45,17 +78,6 @@ git clone https://github.com/santosh/.dotfiles.git
 cd .dotfiles
 make install
 echo Done configuring dotfiles.
-
-# Install bat
-cd /tmp
-LATEST_BAT_64_BIT=$(curl -s https://api.github.com/repos/sharkdp/bat/releases/latest | \
-    grep browser_download_url | grep 'x86_64' | grep musl | \
-    cut -d '"' -f 4)
-curl -sLO $LATEST_BAT_64_BIT
-tar -xvf bat* && cd bat*
-mkdir -p ~/bin && mv bat ~/bin
-cd ~ && rm -r /tmp/bat*
-echo Done install bat.
 
 # Change SSH Port
 
