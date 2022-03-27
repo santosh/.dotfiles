0736b71a69be430781d67cd2c9bc9e90a8844805
Santosh Kumar <sntshkmr60@gmail.com>
2022-03-27 14:50:45 +0000

    Ubuntu bootstrap script for servers

--- bootstrap/ubuntu-server.sh
+++ bootstrap/ubuntu-server.sh
@@ -2,17 +2,42 @@
 
 ##### 0. Update #####
 
-sudo apt update
-sudo apt -y upgrade
+apt update
+apt -y upgrade
 
 
 ##### 1. Installation #####
-sudo apt install make
+apt install make
 
 ##### 1.1 Developer Tools #####
 
-# apt install -y build-essential
+function installDocker() {
+    apt install ca-certificates curl gnupg lsb-release
 
-# golang lags 1 year in LTS version, is it important?
-# sudo apt install golang
-sudo apt install bat
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt update
+    apt install docker-ce docker-ce-cli containerd.io -y
+
+    grep -q -E "^docker:" /etc/group || groupadd docker
+    usermod -aG docker ec2-user
+}
+
+installDocker
+apt install bat
+
+# Set upper limit to journalctl
+journalctl --vacuum-time=180d
+
+echo -n "ENTER a random port number: "
+read SSH_PORT
+if [[ ! $SSH_PORT =~ ^[0-9]+$  ]] ; then
+    echo "SSH port number must be an positive integer."
+    exit
+fi
+sed -i.bak "s/#Port 22/Port $SSH_PORT/g" /etc/ssh/sshd_config
+
+# Install dotfiles
+git clone https://github.com/santosh/.dotfiles.git /home/ubuntu/.dotfiles
