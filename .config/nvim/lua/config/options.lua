bc1784f36c3aa729f84d6a831ef419fdc0007586
Santosh Kumar <sntshkmr60@gmail.com>
2024-06-28 12:18:20 +0530

    disable warnings on every keystroke for java

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,3 +10,21 @@ opt.whichwrap:append({
   ["["] = true,
   ["]"] = true,
 })
+
+return {
+  {
+    "folke/noice.nvim",
+    require("noice").setup({
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      },
+    }),
+  },
+}
